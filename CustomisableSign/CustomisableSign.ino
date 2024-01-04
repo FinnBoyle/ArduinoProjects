f2f13892825749f923b4249e199ffb31500e2144
Finn <finnboyle@icloud.com>
2024-01-04 20:04:23 +1300

    New button setup

--- CustomisableSign/CustomisableSign.ino
+++ CustomisableSign/CustomisableSign.ino
@@ -1,6 +1,7 @@
 #include <LedControl.h>
-#define VRX_PIN A0
-#define VRY_PIN A1
+#define CONF_BUTTON 7
+#define LEFT_BUTTON 4
+#define RIGHT_BUTTON 2
 
 // Led Matrix setup
 int DIN = 12;
@@ -8,10 +9,6 @@ int CS = 10;
 int CLK = 11;
 
 LedControl lc = LedControl(DIN, CLK, CS, 0);
-
-// joystick setup
-int xRead = 0;
-int yRead = 0;
 
 //letters = []; NEEDS TYPE
 
@@ -22,18 +19,38 @@ void setup() {
   lc.shutdown(0, false);
   lc.setIntensity(0, 8);
   lc.clearDisplay(0);
+
+  pinMode(CONF_BUTTON, INPUT_PULLUP);
+  pinMode(LEFT_BUTTON, INPUT_PULLUP);
+  pinMode(RIGHT_BUTTON, INPUT_PULLUP);
 }
+
+int i = 0;
+int j = 0;
+const int MAX = 7;
+const int MIN = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //lc.setLed(0, 0, 0, true);
-
-  xRead = analogRead(A0);
-  yRead = analogRead(A1);
-  Serial.print("this is x: ");
-  Serial.println(xRead);
-  Serial.print("this is y: ");
-  Serial.println(yRead);
-  Serial.println();
-  delay(2000);
+  /*
+  lc.setLed(0, 0, 0, true);
+  delay(500);
+  lc.setLed(0, 0, 0, false);
+  delay(500);
+  */
+  if(digitalRead(CONF_BUTTON) == 0) {
+    //increment vertical 'j' value. If at maximum, reset to minimum
+    j = (j + 1) % (MAX + 1);
+  } else if (digitalRead(LEFT_BUTTON) == 0) {
+    //increment the horizontal 'i' value. If at maximum, increment vertical 'j' instead
+    i = (i + 1) % (MAX + 1);
+    if (i == MIN) j = (j + 1) % (MAX + 1);
+  } else if (digitalRead(RIGHT_BUTTON) == 0) {
+    //decrement the horizontal 'i' value. If at minimum, decrement vertical 'j' instead
+    i = (i - 1 + (MAX + 1)) % (MAX + 1);
+    if (i == MAX) j = (j - 1 + (MAX + 1)) % (MAX + 1);
+  }
+  lc.setLed(0, i, j, true);
+  lc.setLed(0, i, j, false);
+  delay(100);
 }
